dad8a6f0b451be900ea7671f0cd183510d381aba
Oliver-Tobias Ripka <contact@oripka.de>
2021-05-26 10:44:40 +0200

    dsf

--- epan/dissectors/packet-frame.c
+++ epan/dissectors/packet-frame.c
@@ -930,25 +930,22 @@ dissect_frame(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tree, void* 
 	}
 
 	/* Attempt to (re-)calculate color filters (if any). */
-	printf("Before colorization\n");
-	if (pinfo->fd->need_colorize) {
-		printf("Right before match code\n");
-
-		pinfo->fd->nummatched = 0;
-
-		if(evaluate_all_colorrules){
-			printf("Evaluating all colorrules\n");
-			color_filter = color_filters_all_colorize_packet(fr_data->color_edt, pinfo->fd->colorrules_matched, &(pinfo->fd->nummatched), MAX_COLORRULES_MATCHED);
-		} else {
-			printf("Evaluating only one rule\n");
-			color_filter = color_filters_colorize_packet(fr_data->color_edt);
-		}
-		pinfo->fd->color_filter = color_filter;
-		pinfo->fd->need_colorize = 0;
+	
+	//if (pinfo->fd->need_colorize) {
+	printf("Right before match code\n");
+	pinfo->fd->nummatched = 0;
+	if(evaluate_all_colorrules){
+		printf("Evaluating all colorrules\n");
+		color_filter = color_filters_all_colorize_packet(fr_data->color_edt, pinfo->fd->colorrules_matched, &(pinfo->fd->nummatched), MAX_COLORRULES_MATCHED);
 	} else {
-		printf("No need to recolorize\n");
-		color_filter = pinfo->fd->color_filter;
+		printf("Evaluating only one rule\n");
+		color_filter = color_filters_colorize_packet(fr_data->color_edt);
 	}
+	pinfo->fd->color_filter = color_filter;
+	pinfo->fd->need_colorize = 0;
+	//} else {
+	//	color_filter = pinfo->fd->color_filter;
+	//}
 
 	printf("Number of matches %u\n",pinfo->fd->nummatched );
 	if(pinfo->fd->nummatched > 0){
@@ -963,6 +960,7 @@ dissect_frame(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tree, void* 
 			}
 
 			wmem_strbuf_append_printf(val2, "%u", pinfo->fd->colorrules_matched[i]);
+
 		}
 
 		ensure_tree_item(fh_tree, 1);
