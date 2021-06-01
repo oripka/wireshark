075b7ddc7eb7374ea34cb2c0261f107f2f6837f5
Oliver-Tobias Ripka <contact@oripka.de>
2021-06-01 13:44:50 +0200

    sd

--- sharkd.c
+++ sharkd.c
@@ -377,13 +377,13 @@ load_cap_file(capture_file *cf, int max_packet_count, gint64 max_byte_count, int
         snprintf(progressbuf, PROGRESS_BUFFER_SIZE, "{\"progress\" : %ld}\n", nump);
         if (send(output_file, progressbuf, strlen(progressbuf), 0) == -1) {
           perror("[-] Client disconnected, exiting process.");
-          close(output_file)
+          close(output_file);
           exit(1);
         }
 
         if (recv(connection,&peekbuffer,1,MSG_PEEK) < 1) {
           perror("[-] Client disconnected reading, exiting process.");
-          close(output_file)
+          close(output_file);
           exit(1);
         }
 
