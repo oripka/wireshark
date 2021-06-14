e9c3e98d425e00f06b636575c248c4fe602b0879
Oliver-Tobias Ripka <contact@oripka.de>
2021-06-14 08:25:20 +0200

    df

--- ui/clopts_common.c
+++ ui/clopts_common.c
@@ -380,7 +380,7 @@ parse_frame_range(const char *buf, const jsmntok_t *tokens, int count, struct se
 	}
 
 	pch = strtok(tok_frames, " ");
-	while (pch != NULL && (g_ascii_isdigit(pch) || pch == "-"))
+	while (*pch != NULL && (g_ascii_isdigit(pch) || *pch == "-"))
 	{
 		// printf("%s\n", pch);
 		add_selection_new(pch, &maxs, &current, selections, selectionlen);
