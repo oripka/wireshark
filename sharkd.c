f9ec410b52ed80d50b6cf34a1f25e8185fe4a1f3
Oliver-Tobias Ripka <contact@oripka.de>
2022-04-19 16:54:40 +0200

    Add debug output

--- sharkd.c
+++ sharkd.c
@@ -386,19 +386,19 @@ load_cap_file(capture_file *cf, int max_packet_count, gint64 max_byte_count, int
     ws_buffer_init(&buf, 1514);
 
     while (wtap_read(cf->provider.wth, &rec, &buf, &err, &err_info, &data_offset)) {
-
+  
       if(nump % STATUS_EVERY_N_PACKETS == 0 && showprogress){
         // undefined id
         snprintf(progressbuf, PROGRESS_BUFFER_SIZE, "{\"jsonrpc\": \"2.0\", \"id\" : 1, \"result\" : { \"status\" : \"loading\",  \"progress\" :  %ld}}\n", nump-1);
 
         if (send(output_file, progressbuf, strlen(progressbuf), 0) == -1) {
-          //fprintf(stderr, "[-] Client disconnected writing, exiting process. Error code %s\n",  strerror(errno));
+          fprintf(stderr, "[-] Client disconnected writing, exiting process. Error code %s\n",  strerror(errno));
           close(output_file);
           exit(1);
         }
 
         if (recv(output_file,&peekbuffer,1,MSG_PEEK | MSG_DONTWAIT) < 1 && errno != EAGAIN) {
-          //fprintf(stderr, "[-] Client disconnected reading, exiting process. Error code %s\n",  strerror(errno));
+          fprintf(stderr, "[-] Client disconnected reading, exiting process. Error code %s\n",  strerror(errno));
           close(output_file);
           exit(1);
         }
@@ -437,15 +437,23 @@ load_cap_file(capture_file *cf, int max_packet_count, gint64 max_byte_count, int
     cf->provider.prev_dis = NULL;
     cf->provider.prev_cap = NULL;
 
-    if(showprogress){
-      // last update 3 -> LOADED
-      snprintf(progressbuf, PROGRESS_BUFFER_SIZE, "{\"jsonrpc\": \"2.0\", \"id\" : 1, \"result\" : { \"status\" : \"loaded\",  \"progress\" :  %ld}}\n", nump-1);
-      if (send(output_file, progressbuf, strlen(progressbuf), 0) == -1) {
-        //fprintf(stderr, "[-] Client disconnected writing, exiting process. Error code %s\n",  strerror(errno));
-        close(output_file);
-        exit(1);
-      }
+
+  if(showprogress && nump-1 != 0){
+    // last update 3 -> LOADED
+    snprintf(progressbuf, PROGRESS_BUFFER_SIZE, "{\"jsonrpc\": \"2.0\", \"id\" : 1, \"result\" : { \"status\" : \"loaded\",  \"progress\" :  %ld}}\n", nump-1);
+    if (send(output_file, progressbuf, strlen(progressbuf), 0) == -1) {
+      fprintf(stderr, "[-] Client disconnected writing, exiting process. Error code %s\n",  strerror(errno));
+      close(output_file);
+      exit(1);
     }
+  } else {
+    snprintf(progressbuf, PROGRESS_BUFFER_SIZE, "{\"jsonrpc\": \"2.0\", \"id\" : 1, \"result\" : { \"status\" : \"error\",  \"progress\" :  %ld}}\n", nump-1);
+    if (send(output_file, progressbuf, strlen(progressbuf), 0) == -1) {
+      fprintf(stderr, "[-] Client disconnected writing, exiting process. Error code %s\n",  strerror(errno));
+      close(output_file);
+      exit(1);
+    }
+  }
 
   }
 
@@ -455,6 +463,7 @@ load_cap_file(capture_file *cf, int max_packet_count, gint64 max_byte_count, int
 
   return err;
 }
+
 
 cf_status_t
 cf_open(capture_file *cf, const char *fname, unsigned int type, gboolean is_tempfile, int *err)
